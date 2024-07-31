daee278b489f4d653c668163c4acf6bf354da6da
Elpanchobanana <167352725+RanslackZ@users.noreply.github.com>
2024-07-30 20:36:05 -0600

    Delete Sistema experto directory

--- Sistema experto/main.py
+++
@@ -1,8 +0,0 @@
-# main.py
-import tkinter as tk
-from interfaz import InterfazSistemaExperto
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = InterfazSistemaExperto(root)
-    root.mainloop()
